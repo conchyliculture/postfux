b88aacf5189c497b6b90866f6fb5dca1c41587dd
renzokuken <monipute@renzokuken.eu>
2016-12-07 15:03:30 +0100

    filtering is now done in its own process

--- filter.rb
+++ filter.rb
@@ -1,52 +1,39 @@
 #!/usr/bin/ruby
 # apt install ruby-mail ruby-zip
+#
+# This is called by Postfix. It uses server.rb to check the mail
+# and we drop or reroute it depending on the badness.
 
+require "net/http"
+require "json"
 require "open3"
 require "syslog/logger"
 
 $SYSLOG = Syslog.open(__FILE__, Syslog::LOG_PID, Syslog::LOG_MAIL)
 
-module Postfux 
-    class FilterError < Exception ; end
-
-    def Postfux.check_zip(string)
-        require "zip"
-        Zip::File.open_buffer(string) do |z|
-            z.each do |entry|
-                case entry.name
-                when /\.vbs$/i
-                    raise FilterError.new("File named /\\.vbs$/ in a zip")
-                when /\.js$/i
-                    raise FilterError.new("File named /\\.js$/ in a zip")
-                end
-            end
-        end
-    end
-
-    def Postfux.filter(input)
-        # Fire all tests here
-        require "mail"
-        mail = Mail.read_from_string(input)
-        if mail.has_attachments?
-            mail.attachments.each do |a|
-                Postfux.check_zip(a.body.to_s)
-            end
-        end
-    end
-end
 
 def do_sendmail(content)
     _stdout, _stderr, status = Open3.capture3("/usr/sbin/sendmail", "-G", "-i", *ARGV, stdin_data:content)
     return status.exitstatus
 end
 
+def send_to_filter(content)
+    begin
+        status = JSON.parse(Net::HTTP.post_form(URI.parse('http://localhost:12345/'), {'mail' => content}).body)["status"]
+        if status != "ok"
+            $SYSLOG.info("Filter said this is a bad email: '#{status}', dropping mail.")
+            return false
+        end
+    rescue Exception => e
+        $SYSLOG.info("Got an error sending mail to filter: '#{e.message}', delivering mail as usual.")
+    end
+    return true
+end
+
 mail_content = STDIN.read()
 
-begin
-    Postfux.filter(mail_content)
-rescue Postfux::FilterError => e
-    $SYSLOG.info("Dropping mail: #{e.message}") 
-    exit 0 # Everything is fine, we need to return 0 otherwise the mail would bounce
+if not send_to_filter(mail_content)
+    exit 0 # Move along, nothing to see there, we need to return 0 otherwise the mail would bounce
 end
 
 return_status = do_sendmail(mail_content)
---
+++ server.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/ruby
+# apt install ruby-mail ruby-zip
+#
+# This is supposed to run all the time.
+# It will run a WEBrick server, accepting POST forms
+# with a "mail" query, containing the content of the mail
+# to check
+#
+# It will either return a JSON encoded
+# {'status': 'ok'}
+# if the mail looks okay
+# {'status': 'reason why this mail sucks'}
+# otherwise
+
+require "json"
+require "webrick"
+require "syslog/logger"
+
+$SYSLOG = Syslog.open(__FILE__, Syslog::LOG_PID, Syslog::LOG_MAIL)
+
+module Postfux
+    class FilterError < Exception ; end
+
+    def Postfux.check_zip(string)
+        require "zip"
+        Zip::File.open_buffer(string) do |z|
+            z.each do |entry|
+                case entry.name
+                when /\.vbs$/i
+                    raise FilterError.new("File named /\\.vbs$/ in a zip")
+                when /\.js$/i
+                    raise FilterError.new("File named /\\.js$/ in a zip")
+                end
+            end
+        end
+    end
+
+    def Postfux.filter(input)
+        # Fire all tests here
+        require "mail"
+        mail = Mail.read_from_string(input)
+        if mail.has_attachments?
+            mail.attachments.each do |a|
+                Postfux.check_zip(a.body.to_s)
+            end
+        end
+    end
+end
+
+server = WEBrick::HTTPServer.new(
+    :Port => 12345,
+    :BindAddress => "127.0.0.1",
+    :DocumentRoot => "/dev/null", 
+    :Logger => WEBrick::Log.new("/dev/null"),
+    :AccessLog => []
+)
+
+trap("INT") {
+        server.shutdown
+}
+
+server.mount_proc '/' do |req, res|
+    mail_body =  req.query["mail"]
+    response = JSON.generate({'status' => 'ok'})
+    begin
+        Postfux.filter(mail_body)
+    rescue Postfux::FilterError => e
+        $SYSLOG.info("Dropping mail: #{e.message}") 
+        response = JSON.generate({'status' => e.message })
+    end
+    res.body = response
+end
+
+server.start
